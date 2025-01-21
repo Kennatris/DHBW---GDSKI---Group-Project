10eb3c1db049880d4a19e7d60038660c32c49e3f
Kennatris <tino.st.2004@gmail.com>
2025-01-21 08:29:32 +0100

    Get RGB for every Pixel in an Array.

--- test.py
+++ test.py
@@ -9,6 +9,23 @@ def load_image(file_path):
         print(f"Error loading image: {e}")
         return None
 
+def get_stuff_from_image(img):
+    if img is None:
+        print("No image to process.")
+        return
+
+    width, height = img.size
+    pixel = [[0 for _ in range(width)] for _ in range(height)]
+    print(f"Width: {width}, Height: {height}")
+
+    for j in range(height):  # Rows
+        for i in range(width):  # Columns
+            pixel[j][i] = img.getpixel((i, j))
+            print(f"Pixel[{j}][{i}]: {pixel[j][i]}")
+
+    print("Image processing completed.")
+
 if __name__ == "__main__":
-    image_path = "test_img_1.png" #path to image
-    load_image(image_path)
+    image_path = "test_img_1.png"  # Path to image
+    image = load_image(image_path)
+    get_stuff_from_image(image)
