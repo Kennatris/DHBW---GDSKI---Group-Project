febe8700973973d555d4dee1f2ee977b7215a63f
Kennatris <tino.st.2004@gmail.com>
2025-01-24 10:32:41 +0100

    loading from black and white images

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as mpl
 
+
 def load_image(file_path):
     try:
         img = Image.open(file_path)
@@ -11,6 +12,7 @@ def load_image(file_path):
     except Exception as e:
         print(f"Error loading image: {e}")
         return None
+
 
 def get_stuff_from_image(img):
     if img is None:
@@ -23,39 +25,48 @@ def get_stuff_from_image(img):
 
     for j in range(height):  # Rows
         for i in range(width):  # Columns
-             pixel[j,i] = int(sum(img.getpixel((i, j))))
+            tmp = img.getpixel((i, j))[0] + img.getpixel((i, j))[1] + img.getpixel((i, j))[2]
+            if tmp >= 255:
+                pixel[i, j] = 0
+            else:
+                pixel[i, j] = 255
+            #pixel[j, i] = img.getpixel((i, j))
             #print(f"Pixel[{j}][{i}]: {pixel[j][i]}")
+
     print(f"Pixels: {pixel}")
     print("Image processing completed.")
     return np.matrix(pixel)
 
+
 def make_matrix(pixel_matrix, img):
-        if img is None:
-            print("No image to process.")
-            return
+    if img is None:
+        print("No image to process.")
+        return
 
-        width, height = img.size
-        kernel = np.matrix([[0,2],[-2,1]])
+    width, height = img.size
+    kernel = np.matrix([[0, 2], [-2, 1]])
 
-        if width % 2 == 0: # dient der anschaulichkeit
-            width = width
-        elif width % 2 != 0:
-            width = width - 1
+    if width % 2 == 0:  # dient der anschaulichkeit
+        width = width
+    elif width % 2 != 0:
+        width = width - 1
 
-        if height % 2 == 0: # dient der anschaulichkeit
-            height = height
-        elif height % 2 != 0:
-            height = height - 1
+    if height % 2 == 0:  # dient der anschaulichkeit
+        height = height
+    elif height % 2 != 0:
+        height = height - 1
 
-        res = np.matrix(np.ones((int(height/2), int(width/2))))
+    res = np.matrix(np.ones((int(height / 2), int(width / 2))))
 
-        #############################################################
-        ##           Hier muss der Kernel über die Matrix.          ##
-        #############################################################
+    #############################################################
+    ##           Hier muss der Kernel über die Matrix.         ##
+    #############################################################
 
-        print(f"res: {res}")
+    print(f"res: {res}")
+
+
 if __name__ == "__main__":
-    image_path = "test_img_1.png"  # Path to image
+    image_path = "0.png"  # Path to image
     image = load_image(image_path)
     pixel_m = get_stuff_from_image(image)
     make_matrix(pixel_m, image)
